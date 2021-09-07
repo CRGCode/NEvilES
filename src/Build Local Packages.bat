1900292465263ee7945625e9c34ca52613658572
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2021-09-07 16:51:25 +1000

    V1.7.1

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -1,4 +1,4 @@
-set version=1.7.0
+set version=1.7.1
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.DataStore.SQL -o ..\Packages 
