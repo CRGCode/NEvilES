000bc1035ba719fb57e36fb426922f3ea8d39857
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2021-09-10 13:14:44 +1000

    Need to bump V1.7.8 so that Appveyor will publish

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -1,4 +1,4 @@
-set version=1.7.6
+set version=1.7.7
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
