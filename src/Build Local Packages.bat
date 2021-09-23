0f2a60b158f7d643af38217958dcab2b3251e9bf
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2021-09-23 14:32:13 +1000

    V2.0.1

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -1,4 +1,4 @@
-set version=2.0.0
+set version=2.0.1
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
