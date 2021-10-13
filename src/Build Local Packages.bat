f58365d17f556256922bb2a623b975b10587d9df
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2021-10-13 15:51:02 +1100

    Bump V2.1.2

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -1,4 +1,4 @@
-set version=2.1.1
+set version=2.1.2
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
