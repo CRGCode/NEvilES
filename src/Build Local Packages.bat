7384d441d10c8878d9b78c4da65d90bde90187b8
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2022-08-30 21:38:14 +1000

    V2.1.5

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -4,4 +4,4 @@ dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.DataStore.SQL -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.DataStore.Marten -o ..\Packages 
-
+-- nuget api key oy2pcbm6xrelkajki5tahqvpwcc7oboe7ahlwqsgjhvbh4
