a77fa3bef94f3790501c57003a25b8abad753cae
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2023-06-01 17:46:18 +1000

    Align versions

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -1,4 +1,4 @@
-set version=3.0.3
+set version=3.0.4
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
 dotnet pack -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
