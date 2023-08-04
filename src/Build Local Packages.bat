ecf67d144d1921b13730645e7fa49093d05448d8
Craig Gardiner <Craig.Gardiner@anglefinance.com.au>
2023-08-04 14:52:59 +1000

    Debug symbols

--- src/Build Local Packages.bat
+++ src/Build Local Packages.bat
@@ -1,7 +1,9 @@
-set version=3.0.6
-dotnet pack -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
-dotnet pack -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
-dotnet pack -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
-dotnet pack -p:PackageVersion=%version% .\NEvilES.DataStore.SQL -o ..\Packages 
-dotnet pack -p:PackageVersion=%version% .\NEvilES.DataStore.Marten -o ..\Packages 
+set version=4.0.2
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion=%version% .\NEvilES.Abstractions -o ..\Packages 
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion=%version% .\NEvilES -o ..\Packages 
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion=%version% .\NEvilES.Testing -o ..\Packages 
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion=%version% .\NEvilES.DataStore.SQL -o ..\Packages 
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion=%version% .\NEvilES.DataStore.Marten -o ..\Packages 
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion=%version% .\src\Outbox\Outbox.Abstractions\Outbox.Abstractions -c Release -o ..\Packages
+
 -- nuget api key oy2pcbm6xrelkajki5tahqvpwcc7oboe7ahlwqsgjhvbh4
